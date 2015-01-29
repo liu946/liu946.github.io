da418d22cb559917166d5983994a1bb79cc0bfa2
liu946 <liuyang570926881@gmail.com>
2015-01-30 01:29:34 +0800

    template big first word

--- js/init.js
+++ js/init.js
@@ -65,10 +65,10 @@
 /* equal to the browser height.
 ------------------------------------------------------ */
 
-   $('header').css({ 'height': $(window).height() });
+   $('header#home').css({ 'height': $(window).height() });
    $(window).on('resize', function() {
 
-        $('header').css({ 'height': $(window).height() });
+        $('header#home').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
    });
 
@@ -79,7 +79,7 @@
 
    $(window).on('scroll', function() {
 
-		var h = $('header').height();
+		var h = $('header#home').height();
 		var y = $(window).scrollTop();
       var nav = $('#nav-wrap');
 
